d17e4640432d45be495ed091c1e0b274d38f4366
Pablo <48063128+psjs97@users.noreply.github.com>
2022-09-01 00:11:16 +0200

    Added boe_notificaciones.py

---
+++ boe_notificaciones.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+# Libraries
+import requests, csv
+from datetime import datetime
+import xmltodict
+import pandas as pd
+
+print('### OBTENCIÓN NUEVAS NOTIFICACIONES BOE ###')
+
+print("Fecha ejecución: %s" % datetime.now())
+
+def get_current_date():
+    current_year = str(datetime.now().year).zfill(4)
+    current_month = str(datetime.now().month).zfill(2)
+    current_day = str(datetime.now().day).zfill(2)
+    current_date = f'{str(current_year)}{str(current_month)}{str(current_day)}'
+    
+    return current_date
+    
+
+def parse_current_date_xml_boe(current_date):
+    xml_boe_url = f'https://www.boe.es/diario_boe/xml.php?id=BOE-S-{current_date}'
+    response = requests.get(xml_boe_url)
+    xml_boe_data = xmltodict.parse(response.text)
+
+    current_date_boe_df = []
+
+    if not 'error' in xml_boe_data.keys():
+        publications = xml_boe_data['sumario']['diario']['seccion'][0]['departamento']
+        if type(publications) is list:
+            for publication in publications:
+                if not type(publication['epigrafe']) is list:
+                    publication_df = pd.json_normalize(publication, max_level = 5)
+                    current_date_boe_df.append(publication_df)
+
+                else:
+                    name = publication['@nombre']
+                    for i in publication['epigrafe']:
+                        publication_df = pd.json_normalize(i, max_level = 5)
+                        publication_df.rename(columns = {'@nombre':'epigrafe.@nombre'}, inplace = True)
+                        publication_df.insert(loc=0, column='@nombre', value=name)
+                        current_date_boe_df.append(publication_df)
+        else:
+            if not type(publications['epigrafe']) is list:
+                publication_df = pd.json_normalize(publications, max_level = 5)
+                current_date_boe_df.append(publication_df)
+
+            else:
+                name = publications['@nombre']
+                for i in publications['epigrafe']:
+                    publication_df = pd.json_normalize(i, max_level = 5)
+                    publication_df.rename(columns = {'@nombre':'epigrafe.@nombre'}, inplace = True)
+                    publication_df.insert(loc=0, column='@nombre', value=name)
+                    current_date_boe_df.append(publication_df)
+    else:
+        return []
+                    
+    return current_date_boe_df
+    
+    
+
+def main():
+    current_date = get_current_date()
+    current_date_boe_df = parse_current_date_xml_boe(current_date)
+    
+    if len(current_date_boe_df) > 0:
+        current_date_boe_df = current_date_boe_df[current_date_boe_df['DEPARTAMENTO'].str.contains("MINISTERIO DEL INTERIOR")] # Filtramos por 'Ministerio del Interior'
+        current_date_boe_df.to_csv('boe_notifications_' + current_date + '.csv',sep=';',index=None) # Escritura resultados
+        print('Encontradas notificaciones. Escribiendo salida en ',output_file)
+    else:
+        print('No se han encontrado nuevas notificaciones para el día de hoy')
