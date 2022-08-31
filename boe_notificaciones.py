8223a7bcc04515993153adb1b53815bb9adc922b
Pablo <48063128+psjs97@users.noreply.github.com>
2022-09-01 00:16:14 +0200

    Updated boe_notificaciones.py

--- boe_notificaciones.py
+++ boe_notificaciones.py
@@ -7,18 +7,17 @@ import xmltodict
 import pandas as pd
 
 print('### OBTENCIÓN NUEVAS NOTIFICACIONES BOE ###')
-
 print("Fecha ejecución: %s" % datetime.now())
 
+# Función para obtener fecha del día actual
 def get_current_date():
     current_year = str(datetime.now().year).zfill(4)
     current_month = str(datetime.now().month).zfill(2)
     current_day = str(datetime.now().day).zfill(2)
     current_date = f'{str(current_year)}{str(current_month)}{str(current_day)}'
-    
     return current_date
     
-
+# Función para parsear web del BOE
 def parse_current_date_xml_boe(current_date):
     xml_boe_url = f'https://www.boe.es/diario_boe/xml.php?id=BOE-S-{current_date}'
     response = requests.get(xml_boe_url)
@@ -33,7 +32,6 @@ def parse_current_date_xml_boe(current_date):
                 if not type(publication['epigrafe']) is list:
                     publication_df = pd.json_normalize(publication, max_level = 5)
                     current_date_boe_df.append(publication_df)
-
                 else:
                     name = publication['@nombre']
                     for i in publication['epigrafe']:
@@ -45,7 +43,6 @@ def parse_current_date_xml_boe(current_date):
             if not type(publications['epigrafe']) is list:
                 publication_df = pd.json_normalize(publications, max_level = 5)
                 current_date_boe_df.append(publication_df)
-
             else:
                 name = publications['@nombre']
                 for i in publications['epigrafe']:
@@ -59,14 +56,13 @@ def parse_current_date_xml_boe(current_date):
     return current_date_boe_df
     
     
-
-def main():
-    current_date = get_current_date()
-    current_date_boe_df = parse_current_date_xml_boe(current_date)
+# Main
+current_date = get_current_date()
+current_date_boe_df = parse_current_date_xml_boe(current_date)
     
-    if len(current_date_boe_df) > 0:
-        current_date_boe_df = current_date_boe_df[current_date_boe_df['DEPARTAMENTO'].str.contains("MINISTERIO DEL INTERIOR")] # Filtramos por 'Ministerio del Interior'
-        current_date_boe_df.to_csv('boe_notifications_' + current_date + '.csv',sep=';',index=None) # Escritura resultados
-        print('Encontradas notificaciones. Escribiendo salida en ',output_file)
-    else:
-        print('No se han encontrado nuevas notificaciones para el día de hoy')
+if len(current_date_boe_df) > 0:
+    current_date_boe_df = current_date_boe_df[current_date_boe_df['DEPARTAMENTO'].str.contains("MINISTERIO DEL INTERIOR")] # Filtramos por 'Ministerio del Interior'
+    current_date_boe_df.to_csv('boe_notifications_' + current_date + '.csv',sep=';',index=None) # Escritura resultados
+    print('Encontradas notificaciones. Escribiendo salida en ',output_file)
+else:
+    print('No se han encontrado nuevas notificaciones para el día de hoy')
